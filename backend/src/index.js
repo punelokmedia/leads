668b5c7addfda13d55442169bdd692dc3486cb66
ritik gupta <shanigupta@gmail.com>
2026-09-22 17:28:12 +0530

    port entry updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,12 @@ import { ENV } from "./Config/env.config.js";
 import { connectDB } from "./Config/db.connection.config.js";
 import { app } from "./app.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 connectDB()
-  .then(
-    app.listen(PORT, () => {
-      console.log(`✅ Server is running on port http://localhost:${PORT}`);
-    }),
-  )
+  .then(() => {
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
   .catch((err) => console.error(err));
